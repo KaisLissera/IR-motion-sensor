e20bcc580fad9d293a359b86b0c0ad1c8e3d433a
Kais Lissera <ka.pohne2014@gmail.com>
2024-02-07 00:06:03 +0300

    Adapting firmware for stm32f072r8t6

---
+++ Bagryanets_fw/board.h
@@ -0,0 +1,15 @@
+/*
+ * board.h
+ *
+ *  Created on: Feb 6, 2024
+ *      Author: KONSTANTIN
+ */
+
+#ifndef BOARD_H_
+#define BOARD_H_
+
+#define SYS_CLK 16000000
+//#define USE_SYSTICK_DELAY 1
+
+
+#endif /* BOARD_H_ */
---
+++ Bagryanets_fw/main.cpp
@@ -0,0 +1,6 @@
+#include <stdint.h>
+#include <stm32f072xb.h>
+
+int main(void){
+	while(1){}
+}
